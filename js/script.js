028c65c089ead493b24534f6d860a49d071542b2
Rustam Yusupov <rustam.yusupov@gmail.com>
2016-04-01 15:49:38 +0500

    Отображение модальных окон

--- js/script.js
+++ js/script.js
@@ -1,9 +1,59 @@
-var menuToggleBtn = document.querySelector(".header__menu-toggle");
+var btnToggleMenu = document.querySelector(".header__menu-toggle");
 var navMenu = document.querySelector(".menu");
+var btnSubmit = document.querySelector(".btn--submit");
+var frmSomethingWrong = document.querySelector(".modal-form--something-wrong");
+var btnSomethingWrong = document.querySelector(".btn--something-wrong");
+var frmFeedbackSent = document.querySelector(".modal-form--feedback-sent");
+var btnFeedbackSent = document.querySelector(".btn--feedback-sent");
+var fldPhone = document.getElementById("phone-field");
+var fldEmail = document.getElementById("email-field");
+var lblPhoneError = document.querySelector(".feedback__phone .feedback__caption--error");
+var lblEmailError = document.querySelector(".feedback__email .feedback__caption--error");
 
-menuToggleBtn.addEventListener("click", menuToggle);
+btnToggleMenu.addEventListener("click", toggleMenu);
+btnSubmit.addEventListener("click", showHideModalFroms);
+btnSomethingWrong.addEventListener("click", showHideModalFroms);
+btnFeedbackSent.addEventListener("click", showHideModalFroms);
 
-function menuToggle() {
+function toggleMenu() {
+
   navMenu.classList.toggle("menu--closed");
-  menuToggleBtn.classList.toggle("header__menu-toggle--closed");
+
+  btnToggleMenu.classList.toggle("header__menu-toggle--closed");
+
+}
+
+function showHideModalFroms(event) {
+
+  if ( validateForm() ) {
+
+    event.preventDefault();
+
+    frmFeedbackSent.classList.toggle("modal-form--show");
+
+    return;
+  }
+
+  frmSomethingWrong.classList.toggle("modal-form--show");
+
+}
+
+// TODO: Допилить, чтобы ошибка оставалась до того момента, пока не будет исправлена
+function validateForm() {
+
+  if ( !fldPhone.checkValidity() ) {
+
+    lblPhoneError.classList.toggle("feedback__caption--show");
+
+    return false;
+  }
+
+  if ( !fldEmail.checkValidity() ) {
+
+    lblEmailError.classList.toggle("feedback__caption--show");
+
+    return false;
+  }
+
+  return true;
 }
